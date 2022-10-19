40f4898330721450a0e69684bd80d03c9df20643
majkl <michal.jira@gmail.com>
2022-10-19 08:41:17 +0200

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,41 @@
+const cookie = document.getElementById("cookie");
+const counter = document.getElementById("counter");
+const clickupgrade = document.getElementById("clickupgrade");
+const autoclicker = document.getElementById("autoclicker");
+let cookies = 0;
+let clickIncrease = 1;
+let clickIncreasePrice = 100;
+
+let autoclickerIncrease = 0;
+let autoclickerPrice = 200;
+
+cookie.onclick = () => {
+  cookies += clickIncrease;
+  counter.innerHTML = cookies;
+};
+
+clickupgrade.onclick = () => {
+  if (cookies >= clickIncreasePrice) {
+    cookies -= clickIncreasePrice;
+    clickIncreasePrice += 100;
+    clickupgrade.innerHTML = `Buy upgrade:${clickIncreasePrice}`;
+    counter.innerHTML = cookies;
+    clickIncrease += 1;
+  }
+};
+
+autoclicker.onclick = () => {
+    if (cookies >= autoclickerPrice) {
+      cookies -= autoclickerPrice;
+      counter.innerHTML = cookies;
+      autoclickerPrice += 100;
+      autoclicker.innerHTML = `Buy autoclicker upgrade: ${autoclickerPrice}`;
+      if (autoclickerIncrease == 0) {
+        setInterval(() => {
+          cookies += autoclickerIncrease;
+          counter.innerHTML = cookies;
+        }, 1000);
+      }
+      autoclickerIncrease++;
+    }
+  };
